b3d68a689a7eea21ab6cf45972ca92ffd2d87ccf
ICAROCAETANO <icaro.caetano@gmail.com>
2026-09-24 22:14:44 -0300

    first commit

---
+++ src/main/java/com/project/poc_java8_Stream_API/PocJava8StreamApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.poc_java8_Stream_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJava8StreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJava8StreamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/poc_java8_Stream_API/PocJava8StreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poc_java8_Stream_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJava8StreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
